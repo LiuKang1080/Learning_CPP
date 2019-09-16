b397b1ee8e6341eb46e181083a7a35a73a0a82fb
Shivakumar Mahakali <mahakalim@yahoo.com>
2019-09-16 16:27:02 -0400

    Removed extra Hello_World.cpp and moved Hello_World_Test.cpp into the main Learning folder

--- Basics_Of_CPP/Hello_World_Test/Hello_World_Test.cpp
+++ Basics_Of_CPP/Hello_World_Test/Hello_World_Test.cpp
@@ -1,6 +1,10 @@
 // Hello world test in C++
+// comments in C++ are with //
+// Multi-line comments in C++ can be done with /*  */
+
 
 #include <iostream>
+
 
 int main() {
     std::cout << "Hello World" << std::endl;
