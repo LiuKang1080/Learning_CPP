84a4c2ea211c7942efcbd7a0dbabd7ec8e51582d
Shivakumar Mahakali <mahakalim@yahoo.com>
2020-02-09 20:18:29 -0500

    Added more comments to 2_Namespaces.cpp

--- Basics_Of_CPP/1_Variables/Sources/2_Namespaces.cpp
+++ Basics_Of_CPP/1_Variables/Sources/2_Namespaces.cpp
@@ -13,9 +13,28 @@ int main() {
     // we use namespaces to tell C++ which function to use.
 
     // If we were to have our own cout function we would need to use our own namespace
-    // ex) shiva::cout 
+    // ex) my_own_namespace::cout 
 
-    // Using the namespace derective
+    // Using the namespace directive: We can use the namespace directive when we don't want to explicitly write out std:: 
+    // everytime we need to use functions from the standard library. 
+    // General syntax: using namespace [name of the namespace];
+    using namespace std;
+
+    // We can now just use cout and cin without calling std:: 
+    cout << "Using the namespace directive";
+
+    // We need to pay attention to what using namespace directives are actually doing, It will bring in the ENTIRE namespace.
+    // In certain programs we may not want to do that. 
+
+    // Qualified Using Namespace variation:
+    /*
+    using std::cout;
+    using std::cin;
+    using std::endl;
+
+    cout << "qualified using namespaces.";
+    */
+    // Using this variation we can use only what we need. 
 
     return 0;
 }
