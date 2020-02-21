a352f2f181d14164632088c761bf069abecfd6f1
Shivakumar Mahakali <mahakalim@yahoo.com>
2020-02-20 21:57:52 -0500

    Added more comments about cin in 2_Namespaces.cpp

--- Basics_Of_CPP/1_Variables/Sources/2_Namespaces.cpp
+++ Basics_Of_CPP/1_Variables/Sources/2_Namespaces.cpp
@@ -51,10 +51,41 @@ int main() {
             - the data could be an undertermined value.
 
             - data entered from the keyboard will only be processed after enter is pressed.
-            - cin extraction uses whitespaces: spaces, tabs, and newlines are considered termination characters.
+            - cin extraction ignores whitespaces: spaces, tabs, and newlines are considered termination characters,
+              but if there is whitespace before and after the data the compiler will ignore the whitespace and only take in the data.
+            - cin >> var_x; cin first writes the data from the keyboard into a buffer and then reads the buffer to store into the variable var_x.
+
+            :
+            int x;
+            int y;
+            double z;
+
+            cout << "Enter a number";
+            cin >> x;
+            cout << "Enter another number";
+            cin >> y;
+            cout << "You entered " << x << " and " << y;
+            
+            // If we entered "100 200" during the first cin the buffer will hold BOTH the numbers cin >> x will take the first number, and 
+            // cin >> y will take the second number from the buffer. The whitespace after the 100 terminates the first cin for x, when the 
+            // second cin is executed the compiler will read the buffer from where it left off and store the second number in the variable y.
+
+            :
+            cout << "Enter a number";
+            cin >> x;
+            cout << "Enter a double";
+            cin >> z;
+
+            // If we only enter 10.5, x will be 10 and z will 0.5. 1 and 0 are numbers but when the decimal point is reached >> will stop reading
+            // the buffer and take 10 and assign it to the variable x, for z the buffer will continue at the decimal and continue to read it until 
+            // it is done. z = 0.5
+
+            :
+            // If we enter the string "Hello" the compiler will try to assign it to the variable x, and z but will fail to do so since x is an integer
+            // and z is a double. we will get unexpected results.
     */
 
     // Standard coding practices is to #include <iostream> and explicilty use the std:: scope resolution operatror for cout and cin.
-     
+
     return 0;
 }
