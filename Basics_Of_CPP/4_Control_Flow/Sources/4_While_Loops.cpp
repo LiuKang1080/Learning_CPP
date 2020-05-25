3e9d5bd21fe9c90655a8b57e18fbd6a62e529486
Shivakumar Mahakali <mahakalim@yahoo.com>
2020-05-25 19:15:00 -0400

    Changed comments in 4_While_Loop.cpp, Added Guess_The_Random_Number.cpp

--- Basics_Of_CPP/4_Control_Flow/Sources/4_While_Loops.cpp
+++ Basics_Of_CPP/4_Control_Flow/Sources/4_While_Loops.cpp
@@ -26,37 +26,13 @@ While Loops:
         }
         i++;                        // i++ is required to prevent an infinite loop
     }
-
-
 */
 
 
 #include <iostream>
-#include <cstdlib>
-// use rand() for random number generation
-#include <iomanip>
 
 
 int main() {
-    // guess the random number
-
-    // general syntax for random number generation: (std::rand() % max_number) + min_number 
-    // ex) int random_value = (std::rand() % 30) + 15;
-    // this will generate a number between the minimum of 15 and the maximum of 30
-
-    // generating values between 0 and 1:
-    // for (int i = 0; i < 10; i++) {
-    //     std::cout << (float)std::rand() / RAND_MAX << " ";
-    // }
-
-    // initialize variables, random number
-    int my_int = 3;
-
-    std::cout << std::fixed << std::setprecision(5);
-    std::cout << static_cast<double>(my_int);
-    
-
-
 
     return 0;
 }
---
+++ Basics_Of_CPP/4_Control_Flow/Sources/Guess_The_Random_Number.cpp
@@ -0,0 +1,34 @@
+// Using while loop to find the random number.
+
+/*
+Random Number Generation:
+    Random Int Number Generation:
+        - general syntax for random number generation: (std::rand() % max_number) + min_number 
+        
+            int random_value = (std::rand() % 30) + 15;
+    
+        - this will generate a number between the minimum of 15 and the maximum of 30
+
+    Random Float Number generation between 0 and 1:
+        - generating random floats:
+        
+        for (int i = 0; i < 10; i++) {
+            std::cout << (float)std::rand() / RAND_MAX << " ";
+        }
+
+        - 
+
+
+*/
+
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+// need to #include <cstdlib> for rand() for random number generation
+
+
+int main() {
+
+    return 0;
+}
