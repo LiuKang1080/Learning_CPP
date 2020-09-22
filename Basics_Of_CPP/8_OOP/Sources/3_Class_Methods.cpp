7ed026004907ece228d4c8ca2cd9349f5dafad2d
Shivakumar Mahakali <mahakalim@yahoo.com>
2020-09-21 20:03:02 -0400

    Added 3_Class_Methods.cpp

---
+++ Basics_Of_CPP/8_OOP/Sources/3_Class_Methods.cpp
@@ -0,0 +1,30 @@
+// Implementing Class Member Methods in C++
+
+/*
+Member Methods:
+    - Methods in classes are like functions.
+    - Member methods have access to member attributes, so we do NOT need to pass in attributes as arguments.
+    - We can implement methods inside class declarations (they will be implicitly inline).
+        - For small methods it is acceptable to place them inside the class declaration, but for larger and more complex methods
+            we can use multple files to define the classes and its methods.
+    - In C++ it is common to seperate a class' specification from its implementations.
+    - We can use seperate files: .h for the class declaration, and .cpp for the class implementation.
+    - Methods can be implemented outside the class declaration (we need to use the scope resolution operator)
+        Class_Name::method_name
+    - Generally we want to seperate 
+
+Implementing Methods Inside Class Declarations:
+    - 
+
+
+
+*/
+
+
+#include <iostream>
+
+
+int main() {
+
+    return 0;
+}
