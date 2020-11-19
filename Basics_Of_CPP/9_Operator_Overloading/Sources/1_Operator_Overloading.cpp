edae14d44bae0ab886e60b8e4451044326b0c929
Shivakumar Mahakali <mahakalim@yahoo.com>
2020-11-19 18:46:08 -0500

    Added 9_Operator_Overloading\Sources folders. Added 1_Operator_Overloading.cpp

---
+++ Basics_Of_CPP/9_Operator_Overloading/Sources/1_Operator_Overloading.cpp
@@ -0,0 +1,32 @@
+// Operator Overloading in C++
+
+/*
+Operator Overloading:
+    - C++ allows overloading of most operators to work with our user defined classes. Intended to make code more readable, and
+        writable.
+    
+    - What is Operator Overloading?
+        - Traditional operators like +, -, *, / can be used with user defined types.
+        - Allows user defined types to behave similar to built-in types.
+
+    - The assignment operator ( = ) is overloaded for us automatically by the compiler. Has defualt operator overloading. We must
+        explicitly define the other operators for overloading.
+
+    - What operators can be overloaded?
+        - Scope Resolution Operator     ::
+        - Conditional Operator          :?
+        - Pointer to member operator    .*
+        - Dot Operator                  .
+        - Sizeof Operator               sizeof
+
+    - Just because an operator can be overloaded doesn't mean it should. We should only do it when it makes sense.
+*/
+
+
+#include <iostream>
+
+
+int main() {
+
+    return 0;
+}
