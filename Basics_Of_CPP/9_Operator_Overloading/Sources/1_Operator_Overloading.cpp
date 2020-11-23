5548f76cb6c035a24363dcfea970505dc77efa75
Shivakumar Mahakali <mahakalim@yahoo.com>
2020-11-23 17:59:16 -0500

    Added more comments to 1_Operator_Overloading.cpp

--- Basics_Of_CPP/9_Operator_Overloading/Sources/1_Operator_Overloading.cpp
+++ Basics_Of_CPP/9_Operator_Overloading/Sources/1_Operator_Overloading.cpp
@@ -20,6 +20,99 @@ Operator Overloading:
         - Sizeof Operator               sizeof
 
     - Just because an operator can be overloaded doesn't mean it should. We should only do it when it makes sense.
+
+Basic Rules of Operator Overloading:
+    - Precedence and Associativity cannot be changed.
+    - 'arity' cannot be changed. (ex cannot make the division operator a unary operator).
+    - Cannot overload operators for primitive data types (ex int, double, float etc).
+    - Cannot create new operators 
+    - [], (), ->, and the assignment operator ( = ) MUST be declared as member methods.
+    - Other operators can be declared as member methods or as global functions.
+
+    - We will be working with a Mystring class, and overloading the operators in the clas:
+
+        =====
+        Mystring.h
+        =====
+
+        #ifndef _MYSTRING_H_
+        #define _MYSTRING_H_
+
+        class Mystring {
+        private:
+            char *str;
+
+        public:
+            Mystring();                         // no args constructor
+            Mystring(const char *str);          // C-style string / literal constructor
+            Mystring(const Mystring &source);   // copy constructor
+            ~Mystring();                        // destructor
+
+            void display() const;               // display method
+            int get_length() const;             // getter method: gets length of string
+            const char *get_str() const;        // getter method: gets string
+        };
+
+        #endif  // _MYSTRING_H_
+
+        =====
+        Mystring.cpp
+        =====
+
+        #include <iostream>
+        #include <cstring>
+
+        #include "Mystring.h"
+
+
+        // no args constructor
+        Mystring::Mystring() 
+            : str {nullptr} {
+            
+            str = new char[1];
+            *str = "\0";
+        }
+
+        // overloaded constructor
+        Mystring::Mystring(const char *s)
+            : str {nullptr} {
+            
+            if (s == nullptr) {
+                str = new char[1];
+                *str = "\0";
+            } else {
+                str = new char[std::strlen(s) + 1];
+                std::strcpy(str, s);
+            }
+        }
+
+        // copy constructor
+        Mystring::Mystring(const Mystring &source)
+            : str{nullptr} {
+
+            str = new char[std::strlen(source.str) + 1];
+            std::strcpy(str, source.str);
+        }
+
+        // destructor
+        Mystring::~Mystring() {
+            delete [] str;
+        }
+
+        // display method
+        void Mystring::display() const {
+            std::cout << str << "\n";
+        }
+
+        // length getter method
+        int Mystring::get_length() const {
+            return std::strlen(str);
+        }
+
+        // string getter method
+        const char *Mystring::get_str() const {
+            return str;
+        }
 */
 
 
