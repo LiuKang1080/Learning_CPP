147528c244753b6ec80d373e0cd36d96b9218ccb
Shivakumar Mahakali <mahakalim@yahoo.com>
2021-07-26 22:46:05 -0400

    Added OpenGL and the CPP_Boost folders

---
+++ Boost_CPP/1_Storing_Variables/main.cpp
@@ -0,0 +1,27 @@
+// C++ Boost, What is it? When to use it?
+
+/*
+What is the C++ Boost Library?
+	- C++ Boost library offers a wide verity of smaller libraries dedicated to a certain section of C++
+	- Boost offers a wide verity of functions and methods that naturally work with and extend the C++ standard.
+		- We can convert data to and from std::string
+		- We can extend shorts, ints, longs, floats, and doubles into unbounded very large numbers
+		- Boost also provides libraries for Multithreading (mutex, atmoics), and Async.
+		- Managing resources like std::unique_ptr, and std::shared_ptr
+		- Boost also offers image manipulation, and graphing capabilities.
+		- Access to directories, timers, files, and networking services.
+
+When to use the C++ Boost library?
+	- The Boost libraries won't get outdated since more and more boost libraries are becoming part of the C++ standard.
+	- We use the Boost libraries when we want to extend the C++ standard.
+
+*/
+
+
+#include <iostream>
+
+
+int main() {
+
+	return 0;
+}
